1ff1c1999c9d545567fc6f0550489f3677963b68
ryscco <ryscco2@uw.edu>
2021-04-09 22:20:18 -0700

    mech 4 commit

---
+++ Mechanic 4/reflect_off_walls_2d/Assets/Bounce.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb2d;
+    private Vector3 lastVelocity;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = speed * Random.insideUnitCircle.normalized;
+    }
+
+    void Update()
+    {
+        lastVelocity = rb2d.velocity;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb2d.rotation += 2;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rb2d.rotation -= 2;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+        rb2d.velocity = direction * Mathf.Max(speed, 0f);
+    }
+}
